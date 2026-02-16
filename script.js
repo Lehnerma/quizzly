7acc9ccab17de80f301626be043f3d1981a61cd6
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2026-02-16 07:52:10 +0100

    fix the logic in the game for the HTML questions

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let currentQuestion = 0;
-let right_answers = 0;
+let RIGHT_ANSWERS = 0;
 
 const renderCurrentAmount = () => (document.getElementById("currentAmount").innerText = currentQuestion + 1);
+
 const renderQuestion = () => (document.getElementById("question").innerText = QUESTIONS[currentQuestion].question);
+
+const calculateBar = () => (currentQuestion / QUESTIONS.length) * 100;
+
+const gameIsOver = () => currentQuestion >= QUESTIONS.length - 1;
 
 function init() {
   renderAmount();
@@ -12,8 +17,9 @@ function init() {
 }
 
 function renderAmount() {
-  const FULL_AMOUNT = document.getElementById("fullAmount");
-  FULL_AMOUNT.innerText = QUESTIONS.length;
+  document.getElementById("fullAmount").innerText = QUESTIONS.length;
+  document.getElementById("right_answers").innerText = RIGHT_ANSWERS;
+  document.getElementById("end_fullAmount").innerText = QUESTIONS.length;
 }
 
 function renderAnswers() {
@@ -43,6 +49,7 @@ function checkAnswer(answer) {
   let idRightAnswer = `answer_${rightAnswer}`;
   if (rightAnswer == selectAnswer) {
     document.getElementById(answer).parentElement.classList.add("bg-success");
+    RIGHT_ANSWERS++;
   } else {
     document.getElementById(answer).parentNode.classList.add("bg-danger");
     document.getElementById(idRightAnswer).parentNode.classList.add("bg-success");
@@ -67,13 +74,33 @@ function resetAnswers() {
 }
 
 function nextQuestion() {
-  if (currentQuestion >= (QUESTIONS.length -1)) {
-    document.getElementById("quiz--screen").classList.add("d-none");
-    document.getElementById("quiz--end").classList.remove("d-none");
+  if (gameIsOver()) {
+    toogleEndCard();
+    resetAnswers();
   } else {
     resetAnswers();
     disabledBtns();
     countUp();
-    init();
+    renderProgrssbar();
   }
+  init();
+}
+
+function renderProgrssbar() {
+  let bar = document.getElementById("progress_bar");
+  bar.innerText = calculateBar() + "%";
+  bar.style.width = calculateBar() + "%";
+}
+
+function toogleEndCard() {
+  document.getElementById("quiz--screen").classList.toggle("d-none");
+  document.getElementById("quiz--end").classList.toggle("d-none");
+}
+
+function restart() {
+  currentQuestion = 0;
+  RIGHT_ANSWERS = 0;
+  toogleEndCard();
+  init();
+  renderProgrssbar();
 }
